983838f0f50a4557caed30a03011936ac57fa8fa
gyda13 <gyda2100@gmail.com>
2024-02-05 16:47:44 +0300

    fix: save users movie

--- src/main/java/com/lulugyda/repositories/MoviesCrudRepository.java
+++ src/main/java/com/lulugyda/repositories/MoviesCrudRepository.java
@@ -2,10 +2,15 @@ package com.lulugyda.repositories;
 
 import com.lulugyda.models.entities.MovieEntity;
 import io.micronaut.data.annotation.Repository;
-import io.micronaut.data.jdbc.annotation.JdbcRepository;
-import io.micronaut.data.model.query.builder.sql.Dialect;
+import io.micronaut.data.jpa.annotation.EntityGraph;
 import io.micronaut.data.repository.CrudRepository;
+
+import java.util.Optional;
 
 @Repository
 public interface MoviesCrudRepository extends CrudRepository<MovieEntity, Integer> {
+
+    @EntityGraph(attributePaths = {"users"})
+    Optional<MovieEntity> findByMovieId(Integer movieId);
+
 }
--- src/main/java/com/lulugyda/repositories/UsersCrudRepository.java
+++ src/main/java/com/lulugyda/repositories/UsersCrudRepository.java
@@ -2,8 +2,15 @@ package com.lulugyda.repositories;
 
 import com.lulugyda.models.entities.UserEntity;
 import io.micronaut.data.annotation.Repository;
+import io.micronaut.data.jpa.annotation.EntityGraph;
 import io.micronaut.data.repository.CrudRepository;
+
+import java.util.Optional;
 
 @Repository
 public interface UsersCrudRepository extends CrudRepository<UserEntity, Integer> {
+
+    @EntityGraph(attributePaths = {"movieEntity"})
+    Optional<UserEntity> findById(Integer userId);
+
 }
